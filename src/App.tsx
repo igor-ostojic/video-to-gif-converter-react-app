1e3507bd2ae551d5932e53e7375f207c81cc2cf9
Igor Ostojic <igorostojic1996@gmail.com>
2022-06-20 01:10:46 +0200

    downgrade ffmpeg core to 0.10.0

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 
+import { createFFmpeg, fetchFile } from "@ffmpeg/ffmpeg";
+const ffmpeg = createFFmpeg({ log: true });
+
 function App() {
+  const [ready, setReady] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const load = async () => {
+      setLoading(true);
+      await ffmpeg.load();
+      setReady(true);
+      setLoading(false);
+    };
+
+    load();
+  }, []);
+
   return (
     <div className="App">
+      {loading && <div>LOADING...</div>}
       <div>test</div>
     </div>
   );
