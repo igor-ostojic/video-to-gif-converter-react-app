ecac8b7ce2f8d1ff9f0884904d2c66333323305a
Igor Ostojic <igorostojic1996@gmail.com>
2022-06-20 23:42:58 +0200

    add header

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import Header from "./components/Header";
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
   <React.StrictMode>
+    <Header />
     <App />
   </React.StrictMode>
 );
-
