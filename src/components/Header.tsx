a9db706f2278c7e82ace954b0a124abac7428586
Igor Ostojic <igorostojic1996@gmail.com>
2022-06-21 11:53:19 +0200

    update header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,15 @@
+import Logo from "../assets/vgif-logo.png";
+
 const Header = () => {
-  return <header>header</header>;
+  return (
+    <header>
+      <nav>
+        <div className="logo-container">
+          <img src={Logo} alt="Logo" className="logo" />
+        </div>
+      </nav>
+    </header>
+  );
 };
 
 export default Header;
