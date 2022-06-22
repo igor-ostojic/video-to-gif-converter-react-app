652d1e95f6133bcf620db743967694db82fc04eb
Igor Ostojic <igorostojic1996@gmail.com>
2022-06-22 19:40:43 +0200

    animate dobule arrow icon

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,18 @@ import MultiRangeSlider from "./components/MultiRangeSlider";
 
 const ffmpeg = createFFmpeg();
 
+const doubleArrowAnimation = {
+  hidden: { opacity: 0, y: -100 },
+  show: {
+    opacity: 1,
+    y: 0,
+    transition: {
+      ease: [0.6, 0.01, -0.05, 0.95],
+      duration: 1.6,
+    },
+  },
+};
+
 function App() {
   const [loading, setLoading] = useState(false);
   const [video, setVideo] = useState<File | null>(null);
@@ -101,7 +113,7 @@ function App() {
           <div>
             {video && (
               <motion.button
-                initial={{ y: -25, opacity: 0 }}
+                initial={{ y: 10, opacity: 0 }}
                 animate={{
                   y: 0,
                   opacity: 1,
@@ -141,6 +153,29 @@ function App() {
                 console.log("max is " + max);
               }}
             />
+          )}
+          {gif && (
+            <motion.div
+              className="double-arrow-container"
+              variants={doubleArrowAnimation}
+              initial="hidden"
+              animate="show"
+            >
+              <svg
+                width="15"
+                height="15"
+                viewBox="0 0 15 15"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M3.85355 2.14645C3.65829 1.95118 3.34171 1.95118 3.14645 2.14645C2.95118 2.34171 2.95118 2.65829 3.14645 2.85355L7.14645 6.85355C7.34171 7.04882 7.65829 7.04882 7.85355 6.85355L11.8536 2.85355C12.0488 2.65829 12.0488 2.34171 11.8536 2.14645C11.6583 1.95118 11.3417 1.95118 11.1464 2.14645L7.5 5.79289L3.85355 2.14645ZM3.85355 8.14645C3.65829 7.95118 3.34171 7.95118 3.14645 8.14645C2.95118 8.34171 2.95118 8.65829 3.14645 8.85355L7.14645 12.8536C7.34171 13.0488 7.65829 13.0488 7.85355 12.8536L11.8536 8.85355C12.0488 8.65829 12.0488 8.34171 11.8536 8.14645C11.6583 7.95118 11.3417 7.95118 11.1464 8.14645L7.5 11.7929L3.85355 8.14645Z"
+                  fill="currentColor"
+                  fillRule="evenodd"
+                  clipRule="evenodd"
+                ></path>
+              </svg>
+            </motion.div>
           )}
           {gif && <Gif gif={gif} />}
           {gif && (
